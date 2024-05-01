7ce97858036955c8519a1d0f6a5185b42d0850df
anand-kalsait <anand1124134@gmail.com>
2024-05-01 11:05:49 +0530

    looing and conditioning in html in view.py file

--- Django/django_project/django_project/views.py
+++ Django/django_project/django_project/views.py
@@ -1,6 +1,23 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+def home_page(request):
+    data = {
+        "title":"Home_Page",
+        "bdata":"Welcome to the Django Project......",
+        "names":["Anand","Pratik","Prashant","Shruti","Sharad"],
+        "details":[
+            {"name":"Anand", "education":"CS Engineering"},
+            {"name":"Pratik", "education":"Civil Engineering"},
+            {"name":"Prashant", "education":"12th"},
+            {"name":"Shruti", "education":"10th"},
+            {"name":"Sharad", "education":"9th"}
+        ],
+        "numbers":[j for j in range(1,101)]
+
+    }
+    return render(request, "index.html",data)
+
 def about_us(request):
     return HttpResponse("FUCK YOU!")
 
@@ -8,5 +25,3 @@ def about_us(request):
 def courses(request,courseid):
     return HttpResponse(courseid)
 
-def home_page(request):
-    return render(request, "index.html")
